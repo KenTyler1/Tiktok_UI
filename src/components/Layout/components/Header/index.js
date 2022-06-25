572b1bd24be4438bc940378657188c85d29a5f7d
KenTyler1 <tonykhanh999@gmail.com>
2022-06-25 10:58:04 +0700

    Dựng khung DefaultLayout

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames';
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>Header</h2>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>Header pages</div>
+        </header>
+    );
 }
 
 export default Header;
